7b714091994da12ad64ebd66b3ce95ff397aa1c3
arnav <arnavsingla06@gmail.com>
2025-06-05 16:40:27 +0530

    nn

---
+++ strings/stringFunctions.py
@@ -0,0 +1,12 @@
+#concatnation - adding two strings
+
+str1 = "Hello"
+str2 = "world"
+
+str3= str1+" "+str2
+
+print(str3)
+
+print(len(str1),len(str2),len(str3))
+
+#len - used to find string length
